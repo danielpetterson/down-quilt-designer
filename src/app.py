d2d1acc6cf2a9f6ab956503d4ade2e13401d80fa
danielpetterson <dpet922@hotmail.com>
2024-04-23 12:44:39 +1200

    Last dash app update

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from dash import *
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 from functools import reduce
 import operator
@@ -16,8 +16,7 @@ from shapely.geometry import Polygon # type: ignore
 # Change shown units to show width from opposing point, length of section
 # Create df for inner/outer coords (x,y,layer)
 # Configure lower plots to facet based on layer
-# Volume equations based on non-differential (baffle height*area)
-# Calculate amount of fill needed based on input (overstuff, FP)
+# Check adjusted down measurement
 
 ## Later
 # volume for differential. Calculate per baffle?
@@ -26,7 +25,6 @@ from shapely.geometry import Polygon # type: ignore
 GS = 201
 fig = px.scatter(
     x=[0], y=[0]
-# ).update_traces(mode="lines+markers"
                 ).add_traces(
         px.scatter(
             x=np.repeat(np.linspace(-200, 200, GS), GS), y=np.tile(np.linspace(250, 0, GS), GS)
@@ -35,29 +33,25 @@ fig = px.scatter(
         .data
     )
 
+
 fig.layout.height=500
 fig.layout.width=750
 fig.add_vline(x=0, line_width=3, line_dash="dash", line_color="grey")
 fig.update_layout(clickmode="event")
-# # fig.update_layout(clickmode="event+select")
-# # fig.update_layout(dragmode="drawline")
 
 points_selected = []
 
-dataIn = {
+data = {
     'Seam Allowance': [1],
     'Baffle Height':[2.5],
     'Max Chamber Height':[2.5],
     'Number of Chambers': [8],
-    'Down Fill Rating': [800],
+    'Fill Power': [800],
     '% Down Overstuff': [10],
     '% Length with Vertical Baffles': [100],
     'Inner Fabric Weight': [50],
     'Outer Fabric Weight': [50],
     'Baffle Material Weight': [25],
-        }
-
-dataOut = {
     'Length': [0],
     'Width': [0],
     'Area': [0],
@@ -65,11 +59,21 @@ dataOut = {
     'Total Volume': [0],
     'Grams of Down Required': [0],
     'Final Weight': [0]
-            }
+        }
+
+# dataOut = {
+#     'Length': [0],
+#     'Width': [0],
+#     'Area': [0],
+#     'Baffle Width':[0],
+#     'Total Volume': [0],
+#     'Grams of Down Required': [0],
+#     'Final Weight': [0]
+#             }
  
 # Convert the dictionary into DataFrame 
-specsIn = pd.DataFrame(dataIn)
-specsOut = pd.DataFrame(dataOut)
+specs = pd.DataFrame(data)
+# specsOut = pd.DataFrame(dataOut)
  
 
 
@@ -99,20 +103,16 @@ app.layout = html.Div([
     dash.html.H3('Step Two: Draw the right side of the quilt'),
     dcc.Graph(id="graph", figure=fig), 
     html.Div(id="init_plot"),
-    # html.H2('Baffle Construction',id='h2_text'),
     dbc.Container([
         dbc.Label('Click a cell in the table:'),
-        dash_table.DataTable(specsIn.to_dict('records'),[{"name": i, "id": i} for i in specsIn.columns], id='tblIn', editable=True),
-        # dbc.Alert(id='testAlert'),
-    ]),
-    dbc.Container([
-        dash_table.DataTable(specsOut.to_dict('records'),[{"name": i, "id": i} for i in specsOut.columns], id='tblOut', editable=False),
+        dash_table.DataTable(specs.to_dict('records'),[{"name": i, "id": i} for i in specs.columns], id='tblIn', editable=True),
     ]),
     html.Br(),
-    # html.Div([
-    #     "Differential Cut: ",
-    # dcc.RadioItems(['None','Vertical','Horizontal','Both'], 'None', id='diffCut')
-    # ]),
+    dbc.Container([
+        dbc.Label('Click a cell in the table:'),
+        dash_table.DataTable(specs.to_dict('records'),[{"name": i, "id": i} for i in specs.columns], id='tblOut', editable=False),
+    ]),
+    html.Br(),
     dcc.Graph(id="graphBaffle", figure=figBaffle),
     html.Br(),
     dcc.Graph(id="graphFaceted", figure=figInnerOuter),
@@ -149,9 +149,9 @@ def click(clickData, maxHeight, maxWidth):
     # print(x)
     y = [i["y"] for i in points_selected]
     # print(y)
-    length = np.max(y) - np.min(y)
+    # length = np.max(y) - np.min(y)
     # print("Length: ", length)
-    width = np.max(x)*2
+    # width = np.max(x)*2
     # print("Width: ", width)
     x_ref = x + [-i["x"] for i in points_selected]
     y_ref = y + [i["y"] for i in points_selected]
@@ -166,8 +166,8 @@ def click(clickData, maxHeight, maxWidth):
     Input(component_id='tblIn', component_property='data'),
     # Include button press to initiate calcs?
 )
-def updateDataOut(dataIn):
-    print(dataIn[0]['Baffle Height'])
+def updateDataOut(data):
+    # print(data[0]['Baffle Height'])
     if len(points_selected) > 1:
         x = [i["x"] for i in points_selected]
         y = [i["y"] for i in points_selected]
@@ -177,28 +177,44 @@ def updateDataOut(dataIn):
         length = np.max(y) - np.min(y)
         width = np.max(x)*2
         area = PolyArea(x_ref,y_ref)
-        baffleWidth = width/dataIn[0]['Number of Chambers']
-        totalVolume = float(area) * float(dataIn[0]['Baffle Height']) # Only for non-diff cut
-        gramsDown = (totalVolume/dataIn[0]['Down Fill Rating']) 
-        gramsDownAdj = gramsDown * (1 + (dataIn[0]['% Down Overstuff']/100))
+        baffleWidth = float(width)/float(data[0]['Number of Chambers'])
+        totalVolume = float(area) * float(data[0]['Baffle Height']) # Only for non-diff cut
+        FPmet = (float(data[0]['Fill Power']) * 16.387064) / 28.34952 #CUIN/oz to CUCM/g
+        # print(FPmet)
+        gramsDown = (float(totalVolume)/FPmet)
+        gramsDownAdj = gramsDown * (1 + (float(data[0]['% Down Overstuff'])/100))
+        print(length)
+        print(width)
+        print(area)
         print(baffleWidth)
         print(totalVolume)
         print(gramsDown)
         print(gramsDownAdj)
-    return
+        data[0].update({'Length': float(length),
+                         'Width': float(width),
+                         'Area': float(area),
+                         'Baffle Width': float(baffleWidth),
+                         'Total Volume': float(totalVolume),
+                         'Grams of Down Required': float(gramsDownAdj)
+        }
+        )
+        # print(data[0])
+    return data[0]
 
 # Graph Showing Dimensions of Single Full-Width Chamber
 @app.callback(
     Output(component_id='graphBaffle', component_property='figure'),
     Input(component_id='tblIn', component_property='data'),
-    Input(component_id='tblOut', component_property='data'),
+    # Input(component_id='tblOut', component_property='data'),
 )
-def plotBaffleDiagram(dataIn, dataOut):
+def plotBaffleDiagram(dataIn):
     Hb = dataIn[0]['Baffle Height']
     Hc = dataIn[0]['Max Chamber Height']
-    # IWB = dataOut[0]['Width']/dataIn[0]['Number of Chambers']
-    # OWB = (np.sqrt(((IWB/2)**2+(Hc-Hb)**2)*2)/2)*np.pi
-    # print(OWB)
+    print(dataIn[0])
+    IWB = dataIn[0]['Width']/dataIn[0]['Number of Chambers']
+    print(IWB)
+    OWB = (np.sqrt(((IWB/2)**2+(Hc-Hb)**2)*2)/2)*np.pi
+    print(OWB)
     # (SQRT(((H10/2)^2+(C12-C11)^2)*2)/2)*PI()
     # fig.data[0].update(x=x_ref, y=y_ref) 
     return
