1b7f581a03f2daa898fa9a41c9b5126d76c2b392
danielpetterson <dpet922@hotmail.com>
2025-01-26 09:50:13 +1100

    Update app.R
    Added main input fields and controls to add manual vertices

--- DownQuiltDesigner/app.R
+++ DownQuiltDesigner/app.R
@@ -1,9 +1,8 @@
 # pacman::p_load(shiny, tidyverse, shinydashboard, lubridate, scales)
 library(shiny)
+library(bslib)
 library(ggplot2)
 
-# source("R/server.R")
-# source("R/ui.R")
 
 ##TODO:
 # Make mirror x negative
@@ -18,31 +17,47 @@ options(digits=2)
 
 round_any = function(x, accuracy, f=round){f(x/ accuracy) * accuracy}
 
-ui <- pageWithSidebar(
+ui <- shiny::pageWithSidebar(
   headerPanel("Down Quilt Designer"),
   sidebarPanel(
+    # accordion()
+  #   with ui.accordion_panel('Design'):
+  #     # Design
+    numericInput('maxDim','Longest Dimension (cm)', 250, min = 0),
+    numericInput('baffleHeight','Baffle Height (cm)', 2, min = 0),
+    numericInput('chamberHeight','Max Chamber Height (cm)', 2.5, min = 0),
+    numericInput('chamberWidth','Chamber Width (cm)', 15, min = 0),
+    numericInput('percVertBaffle','% Length with Vertical Baffles', 100, min = 0, max = 100),
+  # with ui.accordion_panel('Materials'):
+  #     # Materials
+    numericInput('FP','Fill Power', 750, min = 500, max = 1000, step = 50),
+    numericInput('overstuff','% Overstuff', 10),
+    numericInput('innerWeight','Inner Fabric Weight (gsm)', 50, min = 0),
+    numericInput('outerWeight','Outer Fabric Weight (gsm', 50, min = 0),
+    numericInput('baffleWeight','Baffle Material Weight (gsm)', 25, min = 0),
+    numericInput('seamAllowance','Seam Allowance (cm)', 1, min = 0, step = 0.25)
   ),
   mainPanel(
     fluidRow(column(width = 6,
-                    h4("Enter the length of the quilt"),
-                    numericInput( 
-                      "quilt_length", 
-                      "Quilt Length (cm)", 
-                      value = 200
-                    ), 
                     h4("Click to draw the right half of the quilt"),
+                    verbatimTextOutput("hover_info"),
                     plotOutput("plot1",
                                #add plot click functionality
                                click = "plot_click",
                                #add the hover options
                                hover = hoverOpts(
                                  id = "plot_hover",
-                                 nullOutside = TRUE))),
-             verbatimTextOutput("hover_info"),
+                                 nullOutside = TRUE)),
+                                 h4("Or define vertices manually"),
+                                 shiny::numericInput('x_add','X', 0, min = 0),
+                                 shiny::numericInput('y_add','Y', 0, min = 0)
+                                ),
              column(width = 6,
                     h4("Table of points on plot"),
                     tableOutput("table"))),
-      
+      # button to add vertices
+      actionButton("add_point", "Add Point"),
+      # button to remove last vertex
       actionButton("rem_point", "Remove Last Point"),
 
 
@@ -51,43 +66,47 @@ ui <- pageWithSidebar(
 
 server = function(input, output){
   
-  ## 1. set up reactive dataframe ##
-  values <- reactiveValues()
+  # set up reactive dataframe
+  values <- shiny::reactiveValues()
   values$DT <- data.frame(x = c(0,0),
                           y = c(0,0))
   
-  ## 2. Create a plot ##
-  output$plot1 = renderPlot({
+  # create design plot
+  output$plot1 = shiny::renderPlot({
     ggplot(values$DT, aes(x = x, y = y)) +
       geom_point(aes(), size = 2) +
       geom_path() +
-      lims(x = c(0, 200), y = c(0, input$quilt_length)) +
+      lims(x = c(0, input$maxDim/2), y = c(0, input$maxDim)) +
       theme(legend.position = "bottom")
   })
   
-  ## 3. add new row to reactive dataframe upon clicking plot ##
-  observeEvent(input$plot_click, {
-    # each input is a factor so levels are consistent for plotting characteristics
+  # add new row to reactive dataframe upon clicking plot
+  shiny::observeEvent(input$plot_click, {
     add_row <- data.frame(x = round_any(input$plot_click$x, 0.5),
                           y = round_any(input$plot_click$y, 0.5))
     # add row to the data.frame
     values$DT <- rbind(values$DT[1:nrow(values$DT)-1,], add_row, values$DT[nrow(values$DT),])
-    values$points <- rbind(values$DT, values$DT[nrow(values$DT):1,])
+  })
+
+  # add row on actionButton click
+  shiny::observeEvent(input$add_point, {
+    add_row <- rbind(values$DT, c(input$x_add, input$y_add))
+    values$DT <- add_row
   })
   
-  ## 4. remove row on actionButton click ##
-  observeEvent(input$rem_point, {
+  # remove row on actionButton click
+  shiny::observeEvent(input$rem_point, {
     rem_row <- values$DT[-nrow(values$DT), ]
     values$DT <- rem_row
   })
   
   
-  # 5. render a table of the growing dataframe ##
-  output$table <- renderTable({
-    values$points
+  # render a table of the dataframe
+  output$table <- shiny::renderTable({
+    values$DT
   })
   
-  output$hover_info <- renderPrint({
+  output$hover_info <- shiny::renderPrint({
       hover=input$plot_hover
       cat("X value:", formatC(round_any(hover$x, 0.5), digits = 1, format = "f"), "\n")
       cat("Y value:", formatC(round_any(hover$y, 0.5  ), digits = 1, format = "f"))
@@ -95,4 +114,5 @@ server = function(input, output){
 
 }
 
-shinyApp(ui, server)
+# run app
+shiny::shinyApp(ui, server)
